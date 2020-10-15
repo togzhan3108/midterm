4a41f10af2d543fcf27c7873e8449e6687c4aee6
Togzhan <togzhan3108>
2020-10-15 23:26:06 +0600

    Initial Commit

---
+++ Midterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midterm
+//
+//  Created by Тогжан Салимова on 10/15/20.
+//  Copyright © 2020 Тогжан Салимова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
